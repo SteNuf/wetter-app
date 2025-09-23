0bd6c30e5bcad1359479decc6b6ad7bf3a2fc8fb
SteNuf <stephan.nufer@gmx.de>
2025-09-23 20:36:08 +0200

    The loading screen was created with a fixed time interval. The API was loaded into the current weather in the DOM.

--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,7 @@
 //1. Local Storage erstellen:
-const LOCAL_STORAGE_KEY = "weather-app";
-//console.log(LOCAL_STORAGE_KEY);
+const LOCAL_STORAGE_KEY = "wetter-app";
+
+let weatherApi = "";
 
 //2. API GetWeatherAPI() Verbindung erstellen:
 export async function getWeatherAPI() {
@@ -8,9 +9,45 @@ export async function getWeatherAPI() {
     "http://api.weatherapi.com/v1/current.json?key=cab870990fda438db75125235251909&q=Leipzig&lang=de"
   );
   const body = await response.json();
+  weatherApi = body;
 
-  console.log(body);
+  return {
+    name: body.location.name,
+    temp: body.current.temp_c,
+    condition: body.current.condition.text,
+    heatIndex: body.current.heatindex_c,
+    dewPoint: body.current.dewpoint_c,
+  };
 }
-//3. API in LocalStorage speichern
-//4. API aus LocalStorage herausgeben
+
 //5. API in HTML rendern
+export function renderWeatherText(weather) {
+  document.querySelector(".actually-weather__town").textContent = weather.name;
+  document.querySelector(".actually-weather__temperature").textContent =
+    weather.temp + " °C";
+
+  document.querySelector(".actually-weather__condition").textContent =
+    weather.condition;
+
+  document.querySelector(".actually-weather__max-temperatur").textContent =
+    "H:" + weather.heatIndex + "°";
+  document.querySelector(".actually-weather__min-temperatur").textContent =
+    "T:" + weather.dewPoint + "°";
+}
+
+//3. API in LocalStorage speichern
+export function saveWeatherToLocalStorage() {
+  const weatherData = weatherApi;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(weatherData));
+  console.log(weatherData);
+}
+//4. API aus LocalStorage herausgeben
+export function getWeatherFromLocalStorage() {
+  const data = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (!data) {
+    return [];
+  }
+  console.log(data);
+
+  return JSON.parse(data);
+}
--- src/main.js
+++ src/main.js
@@ -1,7 +1,36 @@
-import { getWeatherAPI } from "./fetch.js";
+import {
+  getWeatherAPI,
+  saveWeatherToLocalStorage,
+  getWeatherFromLocalStorage,
+  renderWeatherText,
+} from "./fetch.js";
 import "../styles/main.scss";
 
-const testButtonElement = document.querySelector(".test-button");
-testButtonElement.addEventListener("click", async () => {
-  const weatherTest = await getWeatherAPI();
+let actuallyWeatherData = "";
+let saveWeatherList = getWeatherFromLocalStorage();
+
+const testLoadButton = document.querySelector(".test-load-button");
+const testSaveButton = document.querySelector(".test-save-button");
+
+testLoadButton.addEventListener("click", async () => {
+  const weather = await getWeatherAPI();
+  renderWeatherText(weather);
+});
+
+testSaveButton.addEventListener("click", async () => {
+  const saveWeather = await saveWeatherToLocalStorage();
+  if (saveWeather) {
+    renderWeatherText(saveWeather);
+  }
+});
+
+// Loading Bildschirm
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("app");
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 1000);
 });
